23cfdb659c3ba5f96f9c0ef92b3ddeba53271a7f
uigormarshall <uigor.silva@acad.ifma.edu.br>
2022-01-19 09:45:18 -0300

    feat: add method boil

---
+++ factory/src/coffe.ts
@@ -0,0 +1,15 @@
+export class Coffe {
+    constructor(_name: string, _type: string){
+        this.name = _name;
+        this.type = _type;
+    }
+    name: string;
+    type: string;
+    watterIsHot: boolean = false;
+
+    boil(): void {
+        console.log('Iniciando fervura...')
+        this.watterIsHot = true;
+        console.log('Fervura Concluida!! \n');
+    }
+}
