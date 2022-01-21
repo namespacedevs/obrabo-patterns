2ebe72fabc36828134f3063569512ceaec849ac8
uigormarshall <uigor.silva@acad.ifma.edu.br>
2022-01-20 21:17:16 -0300

    test: rename test boil

--- factory/src/test/coffe.test.ts
+++ factory/src/test/coffe.test.ts
@@ -3,9 +3,9 @@ import { Coffe } from "../coffe";
 const expect    = require("chai").expect;
 describe("Coffe", function() {
     describe("Boil", function(){
-        it("Boil", function() {
+        it("Should be set watterIsHot to true", function() {
             const coffe = new Coffe('Café Viana', 'Extra Forte');
-            coffe.boil()
+            coffe.boil();
             expect(coffe.watterIsHot).to.equal(true);
         });
     })
