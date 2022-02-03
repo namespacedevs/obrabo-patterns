3b04a659fbdd086192fdca9df938c8c08ad14d14
uigormarshall <uigor.silva@acad.ifma.edu.br>
2022-02-03 11:26:05 -0300

    feat: atualiza index com exemplos de uso

--- factory/ts/src/index.ts
+++ factory/ts/src/index.ts
@@ -1,4 +1,6 @@
 import { Cappucino } from "./cappucino";
+import { CappucinoFabrica } from "./factories/cappucino.fabrica";
+import { MacchiatoFabrica } from "./factories/macchiato.fabrica";
 import { Macchiato } from "./macchiato";
 
 const cappucino = new Cappucino();
@@ -6,3 +8,13 @@ cappucino.preparar();
 
 const macchiato = new Macchiato();
 macchiato.preparar();
+
+const cappucinoFabrica = new CappucinoFabrica();
+const cappucino1 = cappucinoFabrica.preparar();
+
+console.log(cappucino1);
+
+const macchiatoFabrica = new MacchiatoFabrica();
+const macchiato1 = macchiatoFabrica.preparar();
+
+console.log(macchiato1);
