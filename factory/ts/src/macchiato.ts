7cc832f288cc2ebaa34d9cb7acf8df75d4847b8d
uigormarshall <uigor.silva@acad.ifma.edu.br>
2022-02-03 00:01:38 -0300

    feat: implementa o produto Macchiato

---
+++ factory/ts/src/macchiato.ts
@@ -0,0 +1,15 @@
+import { CafeInterface } from "./cafe.interface";
+
+export class Macchiato implements CafeInterface{
+    preparar(): string {
+        const mensagemDeRetorno = `Seu ${Macchiato.name} está pronto!`
+        console.log('Modo de Preparo: \n');
+        console.log('Moa os grãos. \n');
+        console.log('Ponha o pó no porta-filtro. \n');
+        console.log('Faça um expresso. \n');
+        console.log('Vaporize o leite. \n');
+        console.log(' Vire o leite sobre o café e sirva a bebida quente. \n');
+        console.log(mensagemDeRetorno);
+        return mensagemDeRetorno;
+    }
+}
