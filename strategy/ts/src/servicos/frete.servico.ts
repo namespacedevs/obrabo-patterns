e7d0ca5394ca529bec554dd76f33dc8c6874c335
uigormarshall <uigor.silva@acad.ifma.edu.br>
2022-02-04 00:32:58 -0300

    feat: implementa formato de injecao no servico

--- strategy/ts/src/servicos/frete.servico.ts
+++ strategy/ts/src/servicos/frete.servico.ts
@@ -1,11 +1,7 @@
-import { Jadlog } from "./envios/jadlog";
-import { Sedex } from "./envios/sedex";
-import { Fedex } from "./envios/fedex";
+import { EstrategiaInterface } from "./envios/estrategia.interface";
 export class FreteServico{
-    calcula(servico: string, peso: number){
-        if(servico === 'sedex') return Sedex.calcular(peso);
-        if(servico === 'jadlog') return Jadlog.calcular(peso);
-        if(servico === 'fedex') return Fedex.calcular(peso);
-        throw 'Ops! o serviço que você esta tentando consumir não existe.';
+    constructor(private servico: EstrategiaInterface) { }
+    calcula(peso: number){
+        return this.servico.calcular(peso);
     }
 }
