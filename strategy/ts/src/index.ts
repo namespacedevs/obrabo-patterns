8c88d852805e5684d3611a15f84f0d1a8c47dff6
uigormarshall <uigor.silva@acad.ifma.edu.br>
2022-02-04 00:33:38 -0300

    feat: aplica chamada de servico no modelo de injecao

--- strategy/ts/src/index.ts
+++ strategy/ts/src/index.ts
@@ -1,7 +1,7 @@
+import { Sedex } from "./servicos/envios/sedex";
 import { FreteServico } from "./servicos/frete.servico";
 
 console.log('[strategy] aplicação iniciado com sucesso!');
-
-const service = new FreteServico();
-const valorDoFrete = service.calcula('sedex', 1);
+const servico = new FreteServico(new Sedex());
+const valorDoFrete = servico.calcula(1);
 console.log('Valor do frete: ',valorDoFrete,'R$');
