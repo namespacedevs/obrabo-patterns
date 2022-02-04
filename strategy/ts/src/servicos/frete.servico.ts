5994fae9e82ca280db1ade933608043e30bf0371
uigormarshall <uigor.silva@acad.ifma.edu.br>
2022-02-03 23:42:13 -0300

    fix: nome de servico duplicado

--- strategy/ts/src/servicos/frete.servico.ts
+++ strategy/ts/src/servicos/frete.servico.ts
@@ -5,7 +5,7 @@ export class FreteServico{
     calcula(servico: string, peso: number){
         if(servico === 'sedex') return Sedex.calcular(peso);
         if(servico === 'jadlog') return Jadlog.calcular(peso);
-        if(servico === 'sedex') return Fedex.calcular(peso);
+        if(servico === 'fedex') return Fedex.calcular(peso);
         throw 'Ops! o serviço que você esta tentando consumir não existe.';
     }
 }
