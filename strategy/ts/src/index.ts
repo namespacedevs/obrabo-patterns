a87d63b00bee28c32a85a04dd484f986bc829144
uigormarshall <uigor.silva@acad.ifma.edu.br>
2022-02-04 00:41:19 -0300

    refactor: formatacao do index para melhorar apresentacao dos dados

--- strategy/ts/src/index.ts
+++ strategy/ts/src/index.ts
@@ -1,7 +1,18 @@
+import { Fedex } from "./servicos/envios/fedex";
+import { Jadlog } from "./servicos/envios/jadlog";
 import { Sedex } from "./servicos/envios/sedex";
 import { FreteServico } from "./servicos/frete.servico";
 
 console.log('[strategy] aplicação iniciado com sucesso!');
-const servico = new FreteServico(new Sedex());
-const valorDoFrete = servico.calcula(1);
-console.log('Valor do frete: ',valorDoFrete,'R$');
+const servico = new FreteServico(new Fedex());
+const valorFedex = servico.calcula(1);
+
+servico.definirEnvio(new Jadlog());
+const valorJadlog = servico.calcula(1);
+
+servico.definirEnvio(new Sedex())
+const valorSedex = servico.calcula(1);
+
+console.log('[Fedex] Valor do frete: ', valorFedex,'R$. \n');
+console.log('[Jadlog] Valor do frete: ', valorJadlog,'R$. \n');
+console.log('[Sedex] Valor do frete: ', valorSedex,'R$. \n');
