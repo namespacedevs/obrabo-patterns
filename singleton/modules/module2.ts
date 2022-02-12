2fecb321b04513b3d8b75ff13a93f1534114c666
NewJeffiTequila <jeffersoncosta@acad.ifma.edu.br>
2022-02-12 10:27:02 -0300

    reomovendo cometarios do metodo add

--- singleton/modules/module2.ts
+++ singleton/modules/module2.ts
@@ -1,9 +1,9 @@
 import { MyDatabaseClassic } from "../db/my-database-classic";
 import {myDatabaseClassic as myDatabaseCLassicFromModule1} from "./module1"
 const myDatabaseClassic = MyDatabaseClassic.instance;
-// myDatabaseClassic.add({ name: "Sara", age: 21 });
-// myDatabaseClassic.add({ name: "Jeff", age: 21 });
-// myDatabaseClassic.add({ name: "Joao", age: 21 });
+myDatabaseClassic.add({ name: "Uigor ", age: 21 });
+myDatabaseClassic.add({ name: "Rafa", age: 21 });
+myDatabaseClassic.add({ name: "Thiago", age: 21 });
 myDatabaseClassic.remove(1);
 myDatabaseClassic.show();
 console.log(myDatabaseCLassicFromModule1 == myDatabaseClassic)
