1c2d4ff12c23bf8595c57fac29a69faadf4e0f49
Thiago Barros <thiago.barros@bobby.com.br>
2023-10-22 12:46:21 -0300

    feat: executa criacao de objetos

--- builder/php/public/index.php
+++ builder/php/public/index.php
@@ -1,6 +1,33 @@
 <?php
 
+declare(strict_types= 1);
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use src\Casa;
 
-echo 'Construtor pronto para começar.' . PHP_EOL;
+$casaBasica = Casa::reset()
+    ->definirFundacao('Alicerce')
+    ->definirAlvenaria('Tijolo baiano')
+    ->definirCobertura('Telha capa-canal');
+
+$casaMedia = Casa::reset()
+    ->definirFundacao('Viga Baldrame')
+    ->definirImpermeabilizacao(false)
+    ->definirAlvenaria('Bloco estrutural')
+    ->definirAcabamento('chapisco-reboco-emboco')
+    ->definirCobertura('Telha colonial');
+
+$casaSuperior = Casa::reset()
+    ->definirFundacao('Radier')
+    ->definirImpermeabilizacao(true)
+    ->definirAlvenaria('Steel Frame e Dry-Wall')
+    ->definirAcabamento('Porcelanato retificado e polido')
+    ->definirCobertura('Telhado sanduiche')
+    ->definirPiscina(true)
+    ->definirJacuzzi(true)
+    ->definirAdega(true);
+
+var_dump($casaBasica);
+var_dump($casaMedia);
+var_dump($casaSuperior);
